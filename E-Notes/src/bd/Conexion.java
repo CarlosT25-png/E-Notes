b0bb8e70a359e7e06d817c709dd175783f936a5d
Carlos Torres <cartorresva@gmail.com>
2021-02-23 22:31:12 -0600

    Se realizo la clase conexion, la cual permite conectar java con sql

---
+++ E-Notes/src/bd/Conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author carto
+ */
+public class Conexion {
+    public static Connection getConexion(){
+        String conexionUrl = "jdbc:sqlserver://localhost:1433;"
+                + "database=ENOTES;"
+                + "user=sa;"
+                + "password=123;"
+                + "loginTimeout=30;";
+        
+        try {
+            Connection con  = DriverManager.getConnection(conexionUrl);
+            return con;
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+            return null;
+        }
+    }
+}
