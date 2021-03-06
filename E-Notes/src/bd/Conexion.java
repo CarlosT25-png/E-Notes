0be347683e262a165294c3f17a8651bf2f9280cd
Carlos Torres <cartorresva@gmail.com>
2021-03-05 23:23:32 -0600

    Creado el login, falta olvidar contraseña

--- E-Notes/src/bd/Conexion.java
+++ E-Notes/src/bd/Conexion.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  * @author carto
  */
 public class Conexion {
-    public static Connection getConexion(){
+    public static Connection getConnection(){
         String conexionUrl = "jdbc:sqlserver://localhost:1433;"
                 + "database=ENOTES;"
                 + "user=sa;"
